628e3671b11f3ed0973f4f4a815847cc0c2453f8
Siddharth Gawas <108471891+sidgawas@users.noreply.github.com>
2023-11-29 03:37:08 +0530

    [TAS-15] Adds API to subscribe and unscribe a topic (#1)
    * TAS-15 Adds API to subscribe and unscribe a topic
    
    * TAS-15 Adds subsciption to notify.all by default

--- push-notification-gateway/src/main/java/com/siddharthgawas/notification_system/push_notification_gateway/service/impl/PushNotificationServiceImpl.java
+++ push-notification-gateway/src/main/java/com/siddharthgawas/notification_system/push_notification_gateway/service/impl/PushNotificationServiceImpl.java
@@ -1,9 +1,15 @@
 package com.siddharthgawas.notification_system.push_notification_gateway.service.impl;
 
+import com.siddharthgawas.notification_system.push_notification_gateway.dto.SubscriptionRequest;
 import com.siddharthgawas.notification_system.push_notification_gateway.service.PushNotificationService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.AbstractMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
@@ -39,25 +45,55 @@ public class PushNotificationServiceImpl implements PushNotificationService {
      */
     private Map<String, AbstractMessageListenerContainer> containers = new HashMap<>();
 
+    /**
+     * The Notify all.
+     */
+    private Exchange notifyAll;
+
+    /**
+     * The Notify topic subscribers.
+     */
+    private Exchange notifyTopicSubscribers;
+
+    /**
+     * Post construct.
+     */
+    @PostConstruct
+    public void postConstruct() {
+        notifyAll = ExchangeBuilder
+                .fanoutExchange("notify.all").durable(true).build();
+        amqpAdmin.declareExchange(notifyAll);
+        notifyTopicSubscribers = ExchangeBuilder
+                .directExchange("notify.topic").durable(true).build();
+        amqpAdmin.declareExchange(notifyTopicSubscribers);
+    }
+
+    /**
+     * Gets notification stream.
+     *
+     * @param subscriptionId the subscription id
+     * @return the notification stream
+     */
     @Override
     public Flux<String> getNotificationStream(final String subscriptionId) {
-        declareAndBindQueue(subscriptionId);
-
+        declareAndBindQueue(subscriptionId, notifyAll, "");
         final var eventEmitter = new DefaultPushNotificationEmitter(subscriptionId);
         final var listener = new MessageListenerAdapter();
         listener.setDelegate(eventEmitter);
         listener.setDefaultListenerMethod("emit");
         listener.setMessageConverter(new SimpleMessageConverter());
-
         final var container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(getQueueName(subscriptionId));
-        container.setMessageListener(listener);
-
         final var containerKey = subscriptionId + "-" + UUID.randomUUID();
-        containers.put(containerKey, container);
-        container.start();
-        return Flux.create(eventEmitter).doOnCancel(() -> {
+
+        return Flux.create(eventEmitter)
+                .doOnSubscribe(subscription -> {
+                    container.setConnectionFactory(connectionFactory);
+                    container.setMessageListener(listener);
+                    containers.put(containerKey, container);
+                    container.setQueueNames(getQueueName(subscriptionId));
+                    container.start();
+                })
+                .doOnCancel(() -> {
                     log.info("Connection {} is being closed.", containerKey);
                     containers.get(containerKey).stop();
                     containers.remove(containerKey);
@@ -65,17 +101,52 @@ public class PushNotificationServiceImpl implements PushNotificationService {
     }
 
     /**
+     * Subscribe topic.
+     *
+     * @param subscriptionId the subscription id
+     * @param request        the request
+     */
+    @Override
+    public void subscribeTopic(final String subscriptionId, final SubscriptionRequest request) {
+        declareAndBindQueue(subscriptionId, notifyTopicSubscribers, request.topicName());
+        declareAndBindQueue(subscriptionId, notifyAll, "");
+    }
+
+    /**
+     * Unsubscribe topic.
+     *
+     * @param subscriptionId the subscription id
+     * @param request        the request
+     */
+    @Override
+    public void unsubscribeTopic(final String subscriptionId, final SubscriptionRequest request) {
+        unbindQueue(subscriptionId, notifyTopicSubscribers, request.topicName());
+    }
+
+    /**
      * Declare and bind queue.
      *
      * @param subscriptionId the subscription id
+     * @param exchange       the exchange
+     * @param routingKey     the routing key
      */
-    private void declareAndBindQueue(final String subscriptionId) {
+    private void declareAndBindQueue(final String subscriptionId, final Exchange exchange, final String routingKey) {
         final var queue = new Queue(getQueueName(subscriptionId));
-        final var exchange = ExchangeBuilder.fanoutExchange("notify.all")
-                .durable(true).build();
-        amqpAdmin.declareExchange(exchange);
         amqpAdmin.declareQueue(queue);
-        amqpAdmin.declareBinding(BindingBuilder.bind(queue).to(exchange).with("").noargs());
+        amqpAdmin.declareBinding(BindingBuilder.bind(queue).to(exchange).with(routingKey).noargs());
+    }
+
+
+    /**
+     * Unbind queue.
+     *
+     * @param subscriptionId the subscription id
+     * @param exchange       the exchange
+     * @param routingKey     the routing key
+     */
+    private void unbindQueue(final String subscriptionId, final Exchange exchange, final String routingKey) {
+        final var queue = new Queue(getQueueName(subscriptionId));
+        amqpAdmin.removeBinding(BindingBuilder.bind(queue).to(exchange).with(routingKey).noargs());
     }
 
     /**
